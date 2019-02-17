ca5a64dfd8f7a7e3027c2898b40139f2b676865f
ABQJuan <friedrichjohn@hotmail.com>
2019-02-17 11:48:22 -0700

    Initial Commit

---
+++ src/components/layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./layout.css";
+import "./bootstrap.min.css";
+
+const Layout = ({ children }) => <>{children}</>;
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired
+};
+
+export default Layout;
